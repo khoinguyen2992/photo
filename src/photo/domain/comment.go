c5bc7cc131fb012ca7b5c5831c3cacf9059b0dc1
Khoi Nguyen <nguyenminhkhoi.pr@gmail.com>
2016-01-05 08:54:18 +0700

    change domain

--- src/photo/domain/comment.go
+++ src/photo/domain/comment.go
@@ -5,7 +5,7 @@ type Comment struct {
 
 	Id             string   `json:"id,omitempty" gorethink:"id,omitempty"`
 	PhotoId        string   `json:"photo_id" gorethink:"photo_id"`
-	AccountId      string   `json:"-" gorethink:"account_id"`
+	AccountId      string   `json:"account_id" gorethink:"account_id"`
 	NotificationId string   `json:"-" gorethink:"notification_id"`
 	Tags           []string `json:"-" gorethink:"tags"`
 	Text           string   `json:"text" gorethink:"text"`
--- src/photo/domain/photo.go
+++ src/photo/domain/photo.go
@@ -5,7 +5,7 @@ type Photo struct {
 
 	Id        string `json:"id,omitempty" gorethink:"id,omitempty"`
 	AccountId string `json:"account_id" gorethink:"account_id"`
-	SaveName  string `json:"-" gorethink:"save_name"`
+	SaveName  string `json:"save_name" gorethink:"save_name"`
 	Uri       string `json:"uri" gorethink:"uri"`
-	IsPrivate bool   `json:"is_private" gorethink:"is_private"`
+	IsPrivate bool   `json:"-" gorethink:"is_private"`
 }
